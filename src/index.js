e15cfcab9d238327aa44129338e9dc9e1073573e
covicz <konieczkowiczp@gmail.com>
2021-12-20 19:31:52 +0100

    Updated app

---
+++ src/index.js
@@ -0,0 +1,52 @@
+let now = new Date();
+let h2 = document.querySelector("h2");
+
+let hours = now.getHours();
+if (hours < 10){
+    hours = `0${hours}`;
+    }
+let minutes = now.getMinutes();
+if (minutes < 10){
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"]
+let day = days[now.getDay()];
+
+h2.innerHTML = `${day} ${hours}:${minutes}`
+
+function showTemperature(response) {
+   document.querySelector("#city").innerHTML = response.data.name;
+   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp)+`°C Today`;
+ 
+}
+
+function search(event){
+event.preventDefault();
+let apiKey = "9b0cb71e79f417ebc7ae00e0534f9e30";
+let city = document.querySelector("#search-city-input").value;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
+
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", search);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
