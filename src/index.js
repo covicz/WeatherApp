fc6f24b1f7ec09b32a559bc4312a11df759abe0d
covicz <konieczkowiczp@gmail.com>
2021-12-29 18:02:13 +0100

    added unit conversion

--- src/index.js
+++ src/index.js
@@ -17,9 +17,10 @@ h2.innerHTML = `${day} ${hours}:${minutes}`
 
 function showTemperature(response) {
    document.querySelector("#city").innerHTML = response.data.name;
-   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp)+`°C Today`;
+   document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature)+`°C|°F`;
    document.querySelector("#humidity").innerHTML = `Humidity: `+ (response.data.main.humidity)+` %`;
    document.querySelector("#wind").innerHTML = 'Wind: '+ Math.round(response.data.wind.speed)+` km/h`;
+   celsiusTemperature = response.data.main.temp;
    let icon = document.querySelector("#icon");
    icon.setAttribute(
     "src",
@@ -27,11 +28,6 @@ function showTemperature(response) {
   );
 }
    
- 
- 
-  
-
-
 
 function search(event){
 event.preventDefault();
@@ -40,14 +36,31 @@ let city = document.querySelector("#search-city-input").value;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
-
 }
+
+function showFahrenheitTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature*9)/5+32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature)+`°C|°F`;
+}
+function showCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature)+`°C|°F`;
+}
+
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("form");
 form.addEventListener("submit", search);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
-
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", showCelciusTemperature);
 
 
 
